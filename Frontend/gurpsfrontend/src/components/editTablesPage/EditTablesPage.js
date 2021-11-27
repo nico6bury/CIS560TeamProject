f01d51cfa77eff2dae788ee11688a4aa78334950
sdiener17 <sdiener@ksu.edu>
2021-11-27 01:08:30 -0600

    Update

--- Frontend/gurpsfrontend/src/components/editTablesPage/EditTablesPage.js
+++ Frontend/gurpsfrontend/src/components/editTablesPage/EditTablesPage.js
@@ -1,14 +1,58 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import CreateTable from "./CreateTable";
+import DisplayAndEditTables from "./DisplayAndEditTables";
+
+let fakeUserData = [
+  {
+    table: "swords",
+    creatingUser: "User1",
+    data: [
+      {
+        name: "wooden sword",
+        id: 1,
+        basePrice: "34.5",
+      },
+      {
+        name: "metal sword",
+        id: 2,
+        basePrice: "100.4",
+      },
+    ],
+  },
+  {
+    table: "animals",
+    creatingUser: "User2",
+    data: [
+      {
+        name: "flamingo",
+        id: 1,
+        basePrice: "5.00",
+      },
+      {
+        name: "tiger",
+        id: 2,
+        basePrice: "6.89",
+      },
+    ],
+  },
+];
 
 export default function EditTablesPage({ userId }) {
+  const [tableData, setTableData] = useState(fakeUserData);
+
   return (
     <PageWrapper>
       <h3>Edit/Add Tables</h3>
+      <DisplayAndEditTables tableData={tableData} />
+      <div className="separaterBottom" />
       <CreateTable userId={userId} />
     </PageWrapper>
   );
 }
 
-const PageWrapper = styled.nav``;
+const PageWrapper = styled.nav`
+  .separaterBottom {
+    margin-bottom: 20px;
+  }
+`;
