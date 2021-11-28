3fbd38921da32a8524f6fa4de6b0db4596accf9c
sdiener17 <sdiener@ksu.edu>
2021-11-28 00:40:36 -0600

    Update

---
+++ Backend/WebAPI/Controllers/OverallController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebAPI.Controllers {
+    [Route("api/[controller]")]
+    public class OverallController : Controller {
+        // GET: api/<controller>
+        [HttpGet]
+        public IEnumerable<string> Get() {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<controller>/5
+        [HttpGet("{id}")]
+        public string Get(int id) {
+            return "value";
+        }
+
+        
+    }
+}
