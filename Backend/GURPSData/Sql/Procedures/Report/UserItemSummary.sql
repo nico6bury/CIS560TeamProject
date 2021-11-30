5493ef0672f35b462ea6c0d2293b2d043ed73409
Nicholas Sixbury <nico6bury@ksu.edu>
2021-11-29 23:47:23 -0600

    update

--- Backend/GURPSData/Sql/Procedures/Report/UserItemSummary.sql
+++ Backend/GURPSData/Sql/Procedures/Report/UserItemSummary.sql
@@ -1,11 +1,11 @@
 CREATE OR ALTER PROCEDURE AppRecords.UserItemSummary
 AS
-SELECT U.UserID, U.Username, COUNT(IC.ItemCategoryID) AS TablesCreated,
-COUNT(I.ItemID) AS ItemsCreated, COUNT(DISTINCT II.GeneratingTableName) AS TablesUsed,
-COUNT(DISTINCT II.InventoryID) AS ItemsGenerated, U.JoinedOn
-FROM AppRecords.[User] U INNER JOIN AppRecords.ItemCategory IC ON
-U.UserID = IC.OwningUserID INNER JOIN AppRecords.ItemSubcategory ISC ON
-ISC.ItemCategoryID = IC.ItemCategoryID INNER JOIN AppRecords.Item I ON
+SELECT U.UserID, U.Username, COUNT(DISTINCT IC.ItemCategoryID) AS TablesCreated,
+COUNT(DISTINCT I.ItemID) AS ItemsCreated, COUNT(DISTINCT II.GeneratingTableName) AS TablesUsed,
+COUNT(II.InventoryID) AS ItemsGenerated, U.JoinedOn
+FROM AppRecords.[User] U LEFT JOIN AppRecords.ItemCategory IC ON
+U.UserID = IC.OwningUserID LEFT JOIN AppRecords.ItemSubcategory ISC ON
+ISC.ItemCategoryID = IC.ItemCategoryID LEFT JOIN AppRecords.Item I ON
 ISC.ItemID = I.ItemID LEFT JOIN GeneratedItems.InventoryItem II ON
 II.OwningUserID = U.UserID
 GROUP BY U.UserID, U.Username, U.JoinedOn
